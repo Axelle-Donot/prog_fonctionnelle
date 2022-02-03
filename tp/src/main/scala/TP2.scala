bf1cf679fc2dcb4f6d2f4a2a7e679e016b7db079
Axelle Donot <axelle.donot@etu.umontpellier.fr>
2022-02-03 16:16:11 +0100

    fin td2

--- tp/src/main/scala/TP2.scala
+++ tp/src/main/scala/TP2.scala
@@ -33,13 +33,13 @@ object TP2Ex2:
 
   def singleton(n: Int): IntSet = (x : Int) => if (n==x) then true else false ;
 
-  def union(s1: IntSet, s2: IntSet): IntSet = (x : Int) => if (s1(x) == s2(x)) then true else false;
+  def union(s1: IntSet, s2: IntSet): IntSet = (x : Int) => s1(x) || s2(x);
 
-  def intersection(s1: IntSet, s2: IntSet): IntSet = ???
+  def intersection(s1: IntSet, s2: IntSet): IntSet = (x : Int) => s1(x) && s2(x);
 
-  def complement(s: IntSet): IntSet = ???
+  def complement(s: IntSet): IntSet = (x:Int) => !s(x);
 
-  def difference(s1: IntSet, s2: IntSet): IntSet = ???
+  def difference(s1: IntSet, s2: IntSet): IntSet = (x : Int) => s1(x) && !s2(x);
 
   /* Pourquoi est-il impossible de définir une fonction qui indique si deux ensembles IntSet sont égaux? Quelles autres
    * opérations ensemblistes ne peut-on pas définir pour IntSet? */
@@ -49,13 +49,12 @@ object TP2Ex3:
 
   /* Sur le même principe, définissez le type des ensembles de pairs ordonnées d'entiers en remplacant le mot-clé
    * 'Nothing' par une définition appropriée. */
-  type PairOfIntSet = Nothing
+  type PairOfIntSet = (a : Int, b: Int) => Boolean;
 
-  def elem(n: Int, m: Int, s: PairOfIntSet): Boolean = ???
+  def elem(n: Int, m: Int, s: PairOfIntSet): Boolean = s(n, m);
 
   /* Définissez le produit cartésien de deux IntSet */
-  def cartesianProduct(s1: TP2Ex2.IntSet, s2: TP2Ex2.IntSet): PairOfIntSet = ???
-
+  def cartesianProduct(s1: TP2Ex2.IntSet, s2: TP2Ex2.IntSet): PairOfIntSet = (n : Int, m: Int) => s1(n) && s2(m);
 
 object TP2Ex4:
 
@@ -63,35 +62,35 @@ object TP2Ex4:
   type RealFunction = Double => Double
 
   /* Définissez l'évaluation d'une fonction f en un point x et les opérations sur les fonctions */
-  def eval(f: RealFunction, x: Double): Double = ???
+  def eval(f: RealFunction, x: Double): Double = f(x);
 
   /* -f */
-  def negate(f: RealFunction): RealFunction = ???
+  def negate(f: RealFunction): RealFunction = (x:Double) =>  -f(x);
 
   /* 1/f */
-  def invert(f: RealFunction): RealFunction = ???
+  def invert(f: RealFunction): RealFunction = (x:Double) =>  1/f(x);
 
   /* f1 + f2 */
-  def add(f1: RealFunction, f2: RealFunction): RealFunction = ???
+  def add(f1: RealFunction, f2: RealFunction): RealFunction  = (x:Double) =>  f1(x) + f2(x);
 
   /* f1 - f2 */
-  def sub(f1: RealFunction, f2: RealFunction): RealFunction = ???
+  def sub(f1: RealFunction, f2: RealFunction): RealFunction = (x:Double) =>  f1(x) - f2(x);
 
   /* f1 * f2 */
-  def mult(f1: RealFunction, f2: RealFunction): RealFunction = ???
+  def mult(f1: RealFunction, f2: RealFunction): RealFunction = (x:Double) =>  f1(x) * f2(x);
 
   /* f1 / f2 */
-  def div(f1: RealFunction, f2: RealFunction): RealFunction = ???
+  def div(f1: RealFunction, f2: RealFunction): RealFunction = (x:Double) =>  f1(x) / f2(x);
 
   /* f2 ∘ f1 (composition de fonctions) */
-  def compose(f1: RealFunction, f2: RealFunction): RealFunction = ???
+  def compose(f1: RealFunction, f2: RealFunction): RealFunction = (x:Double) =>  f2(f1(x));
 
   /* la fonction constante en c */
-  def constant(c: Double): RealFunction = ???
+  def constant(c: Double): RealFunction = (x:Double) =>  c;
 
   /* la fonction associée au monôme coef.x^exponent */
-  def monomial(coef: Double, exponent: Int): RealFunction = ???
+  def monomial(coef: Double, exponent: Int): RealFunction = (x:Double) => coef*Math.pow(x, exponent)
 
   /* la dérivée de f, approximée par la méthode de la différence centrale:
    * f'(x) est approximée par (f(x + delta) - f(x - delta)) / 2.delta */
-  def approximateDerivative(f: RealFunction, delta: Double): RealFunction = ???
+  def approximateDerivative(f: RealFunction, delta: Double): RealFunction = (x:Double) => (f(x + delta) - f(x - delta)) / (2*delta);
